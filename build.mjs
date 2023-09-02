ac0648e250e7b83e9f35c1386be11d584b1179d5
Baptiste Perbos <baptisteperbos@gmail.com>
2023-09-02 21:47:17 +0200

    reset esbuild config for local dev

--- build.mjs
+++ build.mjs
@@ -1,7 +1,7 @@
 import * as esbuild from 'esbuild'
 import svgr from 'esbuild-plugin-svgr'
 
-await esbuild.build({
+const context = await esbuild.context({
     entryPoints: ["app/javascript/*.*"],
     outdir: "app/assets/builds",
     bundle: true,
@@ -12,11 +12,11 @@ await esbuild.build({
     ]
 })
 
-// // Manually do an incremental build
-// await context.rebuild()
+// Manually do an incremental build
+await context.rebuild()
 
-// // Enable watch mode
-// await context.watch()
+// Enable watch mode
+await context.watch()
 
-// // Enable serve mode
-// await context.serve()
+// Enable serve mode
+await context.serve()
